388f165dbfe93471b2bbf3662c94f77aa9b5946c
AdamAlAttrach <adamalattrach@gmail.com>
2024-11-22 21:05:17 -0500

    map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {APIProvider, Map} from '@vis.gl/react-google-maps';
 
 function App() {
   return (
     <div className="App">
+      <APIProvider apiKey={'AIzaSyA9jah1UU9DtuvDAPe4r91IHgbbuhvFrIw'} onLoad={() => console.log('Maps API has loaded.')}>
+        <Map
+        style={{width: '100vw', height: '100vh'}}
+        defaultCenter={{lat: 22.54992, lng: 0}}
+        defaultZoom={3}
+        gestureHandling={'greedy'}
+        disableDefaultUI={true}
+      />
+      </APIProvider>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
